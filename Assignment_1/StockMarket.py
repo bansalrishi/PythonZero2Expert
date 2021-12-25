b29ee066ede689ff99bf0dd6707a79368a856329
Rishi <rishibansal02@gmail.com>
2021-12-25 18:18:55 +0530

    .

--- Assignment_1/StockMarket.py
+++ Assignment_1/StockMarket.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from time import sleep
+import time
 
 class StockMarket():
     __GBCE = { 'TEA': 
@@ -38,6 +38,12 @@ class StockMarket():
         self.__TRRECORD = {}
 
     def cal_dividend_yield(self, stock, price):
+        """
+        Function to Calculate Dividend Yield
+        :param stock: Its the symbol of a stock
+        :param price: Price of the Stock
+        :return: Dividend Yield
+        """
         if stock in self.__GBCE:
             record = self.__GBCE[stock]
             divYield = 0
@@ -48,11 +54,17 @@ class StockMarket():
                     divYield = (record['f_dividend'] * record['par_value']) / price
                 return divYield
             except ZeroDivisionError as Z:
-                print("Stock Price can't be zero")
+                return ("Price can't be zero")
         else:
             raise Exception("Stock Symbol not Found")
             
     def cal_pe_ratio(self, stock, price):
+        """
+        Function to Calculate P/E Ratio
+        :param stock: Its the symbol of a stock
+        :param price: Price of the Stock
+        :return: P/E Ratio if Stock exist in GBCE table
+        """
         if stock in self.__GBCE:
             dividend = self.cal_dividend_yield(stock, price)
             peRatio = dividend / price
@@ -61,6 +73,13 @@ class StockMarket():
             raise Exception("Stock Symbol not Found")
     
     def cal_vol_weighted_price(self, stock, timeslot = 5):
+        """
+        Function to Calculate Volume Weighted Stock Price
+        :param stock: Its the symbol of a stock
+        :param timeslot(default = 5 min): Consider trade in last x minutes
+        :return: Volume Weighted Stock Price if Stock traded in last x minutes
+                 else 0
+        """        
         timenow = datetime.now().timestamp()
         total_quantity = 0
         summation_price_quantity = 0
@@ -75,6 +94,11 @@ class StockMarket():
             return summation_price_quantity / total_quantity
                 
     def cal_geometric_mean(self):
+        """
+        Function to Calculate Geometric Mean
+        :return: Geometric Mean if at least one Stock traded in last x minutes
+                 else 1
+        """
         geometric = 1
         no_of_stock = 0
         for key in self.__GBCE:
@@ -86,6 +110,11 @@ class StockMarket():
             
     
     def validate_mode(self, mode):
+        """
+        Function to Validate whether Mode is either of Buy or Sell
+        :param mode: Mode of transaction in stock
+        :return: True is mode is either buy or sell otherwise False
+        """
         valid_mode = ['sell', 'buy']
         if mode.lower() in valid_mode:
             return True
@@ -93,19 +122,31 @@ class StockMarket():
             return False
                     
     def record_trade(self, stock, quantity, price, mode):
+        """
+        Function to Record Trade Details of Known Stock
+        :param stock: Its the symbol of a stock
+        :param quantity: How much quantity of stock is traded
+        :param price: Price of the Stock
+        :return:  
+        """
         if stock in self.__GBCE and self.validate_mode(mode):
             timenow = datetime.now().timestamp()
+            time.sleep(0.0001)
             self.__TRRECORD[timenow] = {
             'mode' : mode,
             'price': price,
             'volume': quantity,
             'symbol': stock
             }
-            sleep(0.05)
+            print("Stock Record added successfully")
         else:
             print("Not a Valid Stock: {0}".format(stock))
 
     def show_trade_record(self):
+        """
+        Function to print Record Trade Details
+        :return: 
+        """        
         print(self.__TRRECORD)
             
             
@@ -118,5 +159,5 @@ s.record_trade("JOE", 20, 20, "buy")
 s.record_trade("ALE", 500, 100, "sell")
 s.record_trade("GIN", 100, 300, "sell")
 s.show_trade_record()
-print(s.cal_vol_weighted_price("POP"))
+print(s.cal_vol_weighted_price("JOE"))
 print(s.cal_geometric_mean())
